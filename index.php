d733c128be89d97a832cc56f8844e577a876ad98
TheDronIt <TheDronIt@gmail.com>
2018-05-08 21:01:32 +1000

    first

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>Competition</title>
+<link rel="stylesheet" href="/style/style.css">
+<link rel="stylesheet" href="/style/normalize.css">
+<link rel="stylesheet" href="/style/neon.css">
+<link rel="shortcut icon" href="img/icon.png" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+  <div class = "head">
+	<div class = "menu">
+  	  <a href="/"><img src="img/logo.png" class = "menu-logo" ></a>
+	 
+	<a class= "menu-block">текст1 2</a> 
+	  <a class= "menu-block">текст 2</a> 
+	</div>
+	<img src="img/1.jpg" height=90% width= 100%>
+  </div>
+  
+<ul class="content-cards page-content__content-cards">
+
+  <li class="content-cards-item  page-content__cards-item"> <!------ 4 БЛОК ----->
+    <img class="content-cards-item__cover" src="/img/phot.jpg" width="480" alt="">
+    <div class="content-line">
+	  <div class="cards-title">Заголовок</div>
+	</div>
+    <div class="cards-description">Описание</div>
+  </li>
+			
+<li class="content-cards-item  page-content__cards-item"> <!------ 4 БЛОК ----->
+    <img class="content-cards-item__cover" src="/img/phot.jpg" width="480" alt="">
+    <div class="content-line">
+	  <div class="cards-title">Заголовок</div>
+	</div>
+    <div class="cards-description">Описание</div>
+  </li>
+
+<li class="content-cards-item  page-content__cards-item"> <!------ 4 БЛОК ----->
+    <img class="content-cards-item__cover" src="/img/phot.jpg" width="480" alt="">
+    <div class="content-line">
+	  <div class="cards-title">Заголовок</div>
+	</div>
+    <div class="cards-description">Описание</div>
+  </li>  
+	
+</ul>
+</body>
+</html>
